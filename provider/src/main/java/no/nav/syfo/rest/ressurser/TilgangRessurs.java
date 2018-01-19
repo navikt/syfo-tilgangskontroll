1d5aa0d98fe5c2b5e519edc21a73d18322b735ec
Engelien, Stefan Andreas <stefan.andreas.engelien@nav.no>
2018-01-19 09:16:28 +0100

    SYFOUTV-1804 Returner Response i stedet for Tilgang-objekter

--- provider/src/main/java/no/nav/syfo/rest/ressurser/TilgangRessurs.java
+++ provider/src/main/java/no/nav/syfo/rest/ressurser/TilgangRessurs.java
@@ -4,14 +4,18 @@ package no.nav.syfo.rest.ressurser;
 import io.swagger.annotations.Api;
 import no.nav.metrics.aspects.Count;
 import no.nav.metrics.aspects.Timed;
+import no.nav.sbl.util.StringUtils;
 import no.nav.syfo.domain.Tilgang;
 import no.nav.syfo.services.TilgangService;
 import org.springframework.stereotype.Controller;
 
 import javax.inject.Inject;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.FORBIDDEN;
 
 @Path("/tilgang")
 @Consumes(APPLICATION_JSON)
@@ -27,16 +31,35 @@ public class TilgangRessurs {
     @Timed(name = "tilgangTilTjenesten")
     @Count(name = "tilgangTilTjenesten")
     @Path("/tilgangtiltjenesten")
-    public boolean tilgangTilTjenesten() {
-        return tilgangService.harTilgangTilTjenesten();
+    public Response tilgangTilTjenesten() {
+        if(tilgangService.harTilgangTilTjenesten()){
+            return Response.ok().build();
+        } else {
+            return Response.status(BAD_REQUEST)
+                    .build();
+        }
     }
 
     @GET
     @Timed(name = "tilgangTilBruker")
     @Count(name = "tilgangTilBruker")
     @Path("/tilgangtilbruker")
-    public Tilgang tilgangTilBruker(@QueryParam("fnr") String fnr) {
-        return tilgangService.sjekkTilgang(fnr);
+    public Response tilgangTilBruker(@QueryParam("fnr") String fnr) {
+        if (StringUtils.nullOrEmpty(fnr)) {
+            return Response.status(BAD_REQUEST)
+                    .entity("fnr parameter is mandatory")
+                    .build();
+        } else {
+            Tilgang tilgang = tilgangService.sjekkTilgang(fnr);
+            if (tilgang.ikkeTilgang) {
+                return Response.status(FORBIDDEN)
+                        .entity(tilgang)
+                        .type(APPLICATION_JSON)
+                        .build();
+            } else {
+                return Response.ok().build();
+            }
+        }
     }
 
 }
