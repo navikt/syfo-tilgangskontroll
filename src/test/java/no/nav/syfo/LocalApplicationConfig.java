5a453244d4269e1956b7617cf4c727e402736321
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-10-09 11:57:03 +0200

    Organize imports

--- src/test/java/no/nav/syfo/LocalApplicationConfig.java
+++ src/test/java/no/nav/syfo/LocalApplicationConfig.java
@@ -1,18 +1,12 @@
 package no.nav.syfo;
 
 import no.nav.security.oidc.test.support.spring.TokenGeneratorConfiguration;
-import no.nav.syfo.mocks.EgenansattMock;
-import no.nav.syfo.mocks.OrganisasjonEnhetMock;
-import no.nav.syfo.mocks.OrganisasjonRessursEnhetMock;
-import no.nav.syfo.mocks.PersonMock;
+import no.nav.syfo.mocks.*;
 import no.nav.tjeneste.pip.egen.ansatt.v1.EgenAnsattV1;
 import no.nav.tjeneste.virksomhet.organisasjon.ressurs.enhet.v1.OrganisasjonRessursEnhetV1;
 import no.nav.tjeneste.virksomhet.organisasjonenhet.v2.OrganisasjonEnhetV2;
 import no.nav.tjeneste.virksomhet.person.v3.PersonV3;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.*;
 import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestTemplate;
 
@@ -31,7 +25,7 @@ public class LocalApplicationConfig {
     }
 
     @Bean
-    public OrganisasjonEnhetV2 organisasjonEnhetV2Mock () {
+    public OrganisasjonEnhetV2 organisasjonEnhetV2Mock() {
         return new OrganisasjonEnhetMock();
     }
 
