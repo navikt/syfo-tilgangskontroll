8305bdf3d2300aa98fc04d24063510529e5eaab9
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-10-15 08:19:32 +0200

    Organize imports

--- src/main/java/no/nav/syfo/ws/OnBehalfOfOutInterceptor.java
+++ src/main/java/no/nav/syfo/ws/OnBehalfOfOutInterceptor.java
@@ -12,9 +12,7 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 
 import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.*;
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.Base64;
