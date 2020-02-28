ee8758de23fd226bdd5c90e3f16e8ae47520b996
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-02-28 12:07:28 +0100

    Remove conf of OIDC-isseru Intern

--- src/main/java/no/nav/syfo/security/OIDCIssuer.java
+++ src/main/java/no/nav/syfo/security/OIDCIssuer.java
@@ -1,7 +1,6 @@
 package no.nav.syfo.security;
 
 public class OIDCIssuer {
-    public final static String INTERN = "intern";
     public final static String AZURE = "veileder";
     public final static String VEILEDERAZURE = "veilederaad";
 }
