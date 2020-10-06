b5958e46870b39e262f96b3806fe74ac84e10d3b
Erik Jansen <erik.gunnar.jansen@nav.no>
2020-10-06 18:55:58 +0200

    Use kotlin plugin to set version of stdlib and reflect

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,6 @@ val tokenValidationSpringSupportVersion = "1.3.0"
 val personV3Version = "1.2019.07.11-06.47-b55f47790a9d"
 val springBootVersion = "2.2.0.RELEASE"
 val springRetryVersion = "1.2.4.RELEASE"
-val kotlinLibVersion = "1.3.50"
 val kotlinJacksonVersion = "2.10.0"
 val logbackVersion = "6.3"
 
@@ -48,8 +47,8 @@ repositories {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8:$kotlinLibVersion")
-    implementation("org.jetbrains.kotlin:kotlin-reflect:$kotlinLibVersion")
+    implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$kotlinJacksonVersion")
 
     implementation("org.springframework.boot:spring-boot-starter-web:$springBootVersion")
