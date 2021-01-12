44d048a28149894872d54b8a7324e59ea2b53df0
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-12 18:31:58 +0100

    Convert Metric to Kotlin

---
+++ src/main/kotlin/no/nav/syfo/metric/Metric.kt
@@ -0,0 +1,32 @@
+package no.nav.syfo.metric
+
+import io.micrometer.core.instrument.MeterRegistry
+import io.micrometer.core.instrument.Tags
+import org.springframework.stereotype.Component
+import javax.inject.Inject
+
+@Component
+class Metric @Inject constructor(
+    private val registry: MeterRegistry
+) {
+    fun countEvent(name: String) {
+        registry.counter(
+            addPrefix(name),
+            Tags.of("type", "info")
+        ).increment()
+    }
+
+    fun tellHttpKall(kode: Int) {
+        registry.counter(
+            addPrefix("httpstatus"),
+            Tags.of(
+                "type", "info",
+                "kode", kode.toString())
+        ).increment()
+    }
+
+    private fun addPrefix(navn: String): String {
+        val metricPrefix = "syfotilgangskontroll_"
+        return metricPrefix + navn
+    }
+}
