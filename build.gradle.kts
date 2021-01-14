c8aa8cc9f46b357714d73847fa8687d63a9c4464
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-14 13:28:57 +0100

    Remove unused import

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import com.github.jengelman.gradle.plugins.shadow.transformers.PropertiesFileTransformer
-import com.github.jengelman.gradle.plugins.shadow.transformers.ServiceFileTransformer
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 group = "no.nav.syfo"
