ffb1ff8b3aa301a7fca0eade16338493e71f9f02
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-01-19 20:40:57 +0100

    Bump Kotlin version
    Bump Kotlin version 1.4.10 -> 1.4.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,8 +14,8 @@ val slf4jVersion = "1.7.25"
 val tokenValidationSpringSupportVersion = "1.3.0"
 
 plugins {
-    kotlin("jvm") version "1.4.10"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.10"
+    kotlin("jvm") version "1.4.21"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.21"
     id("org.springframework.boot") version "2.3.8.RELEASE"
     id("io.spring.dependency-management") version "1.0.10.RELEASE"
     id("com.github.johnrengelman.shadow") version "6.0.0"
