1f1b626adc400c7dcf99f68b9ae16a6efbcf238f
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-09-09 15:47:08 +0200

    Replace STS-token with AzureAD-token in BehandlendeEnhetConsumer

--- src/main/kotlin/no/nav/syfo/consumer/behandlendeenhet/BehandlendeEnhetConsumer.kt
+++ src/main/kotlin/no/nav/syfo/consumer/behandlendeenhet/BehandlendeEnhetConsumer.kt
@@ -1,8 +1,8 @@
 package no.nav.syfo.consumer.behandlendeenhet
 
 import no.nav.syfo.cache.CacheConfig
+import no.nav.syfo.consumer.azuread.AzureAdTokenConsumer
 import no.nav.syfo.metric.Metric
-import no.nav.syfo.consumer.sts.StsConsumer
 import no.nav.syfo.util.*
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -14,42 +14,50 @@ import org.springframework.web.client.RestTemplate
 
 @Service
 class BehandlendeEnhetConsumer(
+    private val azureAdTokenConsumer: AzureAdTokenConsumer,
     @Value("\${syfobehandlendeenhet.url}") private val baseUrl: String,
+    @Value("\${syfobehandlendeenhet.client.id}") private val syfotilgangskontrollClientId: String,
     private val metric: Metric,
-    private val stsConsumer: StsConsumer,
     private val template: RestTemplate
 ) {
+    private val behandlendeEnhetPersonIdentUrl = "$baseUrl$SYSTEM_V2_PERSONIDENT_PATH"
 
     @Cacheable(cacheNames = [CacheConfig.CACHENAME_BEHANDLENDEENHET_FNR], key = "#fnr", condition = "#fnr != null")
     fun getBehandlendeEnhet(fnr: String, callId: String?): BehandlendeEnhet {
-        val stsToken = stsConsumer.token()
-
-        val httpEntity = entity(callId, stsToken)
+        val oboToken = azureAdTokenConsumer.getSystemToken(
+            scopeClientId = syfotilgangskontrollClientId,
+        )
         try {
-            val response = template.exchange<BehandlendeEnhet>(
-                getBehandlendeEnhetUrl(fnr),
+            val response = template.exchange(
+                behandlendeEnhetPersonIdentUrl,
                 HttpMethod.GET,
-                httpEntity,
+                createEntity(
+                    callId = callId,
+                    personIdentNumber = fnr,
+                    token = oboToken,
+                ),
                 BehandlendeEnhet::class.java
             )
             val responseBody = response.body!!
             metric.countEvent(METRIC_CALL_BEHANDLENDEENHET_SUCCESS)
             return responseBody
         } catch (e: RestClientResponseException) {
-            LOG.error("Error requesting BehandlendeEnhet from syfobehandlendeenhet with callId ${httpEntity.headers[NAV_CALL_ID_HEADER]}: ", e)
+            LOG.error("Error requesting BehandlendeEnhet from syfobehandlendeenhet with callId=$callId: ", e)
             metric.countEvent(METRIC_CALL_BEHANDLENDEENHET_FAIL)
             throw e
         }
     }
 
-    private fun getBehandlendeEnhetUrl(bruker: String) = "$baseUrl/api/$bruker"
-
-    private fun entity(callId: String?, token: String): HttpEntity<String> {
-        val credentials = bearerCredentials(token)
+    private fun createEntity(
+        callId: String?,
+        personIdentNumber: String,
+        token: String,
+    ): HttpEntity<String> {
         val headers = HttpHeaders()
-        headers[HttpHeaders.AUTHORIZATION] = credentials
+        headers.setBearerAuth(token)
         headers[NAV_CALL_ID_HEADER] = getOrCreateCallId(callId)
         headers[NAV_CONSUMER_ID_HEADER] = APP_CONSUMER_ID
+        headers[NAV_PERSONIDENT_HEADER] = personIdentNumber
         return HttpEntity(headers)
     }
 
@@ -58,5 +66,7 @@ class BehandlendeEnhetConsumer(
 
         const val METRIC_CALL_BEHANDLENDEENHET_SUCCESS = "call_behandlendeenhet_success"
         const val METRIC_CALL_BEHANDLENDEENHET_FAIL = "call_behandlendeenhet_fail"
+
+        const val SYSTEM_V2_PERSONIDENT_PATH = "/api/system/v2/personident"
     }
 }
--- src/main/kotlin/no/nav/syfo/util/CredentialUtil.kt
+++ src/main/kotlin/no/nav/syfo/util/CredentialUtil.kt
@@ -5,5 +5,3 @@ import java.util.*
 fun basicCredentials(credentialUsername: String, credentialPassword: String): String {
     return "Basic " + Base64.getEncoder().encodeToString(java.lang.String.format("%s:%s", credentialUsername, credentialPassword).toByteArray())
 }
-
-fun bearerCredentials(token: String) = "Bearer $token"
