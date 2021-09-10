80f344bd10f72d74e1497b5c126e004546c59590
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-09-10 14:31:36 +0200

    Remove unused STSConsumer and STS-logic

--- init.sh
+++ init.sh
@@ -2,6 +2,3 @@ export LDAP_USERNAME=$(cat /secrets/ldap/ldap/username)
 export LDAP_PASSWORD=$(cat /secrets/ldap/ldap/password)
 export AAD_CLIENT_ID=$(cat /var/run/secrets/nais.io/azuread/client_id)
 export AAD_CLIENT_SECRET=$(cat /var/run/secrets/nais.io/azuread/client_secret)
-
-export SRV_USERNAME=$(cat /secrets/serviceuser/syfo-tilgangskontroll/username)
-export SRV_PASSWORD=$(cat /secrets/serviceuser/syfo-tilgangskontroll/password)
