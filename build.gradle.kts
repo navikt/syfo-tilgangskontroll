1fab2018c712b31da8511c40fcf218be17d1a95b
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-12-17 16:36:15 +0100

    Bump dependencies (#95)

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,19 +7,19 @@ version = "1.0.0"
 
 val apacheHttpClientVersion = "4.5.13"
 val javaxActivationVersion = "1.2.0"
-val jaxRiVersion = "2.3.3"
-val kotlinJacksonVersion = "2.11.3"
-val logbackVersion = "6.3"
-val prometheusVersion = "1.6.3"
-val slf4jVersion = "1.7.30"
-val tokenValidationSpringSupportVersion = "1.3.2"
+val jaxRiVersion = "2.3.5"
+val kotlinJacksonVersion = "2.13.0"
+val logbackVersion = "7.0.1"
+val prometheusVersion = "1.8.1"
+val slf4jVersion = "1.7.32"
+val tokenValidationSpringSupportVersion = "1.3.9"
 
 plugins {
-    kotlin("jvm") version "1.4.21"
-    id("org.jetbrains.kotlin.plugin.allopen") version "1.4.21"
-    id("org.springframework.boot") version "2.4.6"
-    id("io.spring.dependency-management") version "1.0.10.RELEASE"
-    id("com.github.johnrengelman.shadow") version "6.0.0"
+    kotlin("jvm") version "1.6.10"
+    id("org.jetbrains.kotlin.plugin.allopen") version "1.6.10"
+    id("org.springframework.boot") version "2.4.13"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("com.github.johnrengelman.shadow") version "7.1.1"
 }
 
 allOpen {
@@ -75,6 +75,9 @@ tasks {
     }
 
     withType<ShadowJar> {
+        archiveBaseName.set("app")
+        archiveClassifier.set("")
+        archiveVersion.set("")
         transform(PropertiesFileTransformer::class.java) {
             paths = listOf("META-INF/spring.factories")
             mergeStrategy = "append"
