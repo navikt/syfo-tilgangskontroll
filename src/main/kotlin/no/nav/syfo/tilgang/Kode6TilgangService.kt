f7684279b949e30754b4ec9123cde891043818ec
geir-waagboe <geir.arne.waagbo@nav.no>
2022-09-13 14:25:38 +0200

    Tillate kode6-tilgang fra syfomodia

--- src/main/kotlin/no/nav/syfo/tilgang/Kode6TilgangService.kt
+++ src/main/kotlin/no/nav/syfo/tilgang/Kode6TilgangService.kt
@@ -9,17 +9,37 @@ import org.springframework.stereotype.Service
 @Service
 class Kode6TilgangService(
     private val adRoller: AdRoller,
-    @Value("\${smregistrering.client.id}") private val smregisteringClientId: String,
+    @Value("\${fastlegerest.client.id}") private val fastlegerestClientId: String,
+    @Value("\${finnfastlege.client.id}") private val finnfastlegeClientId: String,
+    @Value("\${isdialogmelding.client.id}") private val isdialogmeldingClientId: String,
     @Value("\${isdialogmote.client.id}") private val isdialogmoteClientId: String,
+    @Value("\${isdialogmotekandidat.client.id}") private val isdialogmotekandidatClientId: String,
+    @Value("\${isnarmesteleder.client.id}") private val isnarmestelederClientId: String,
+    @Value("\${isoppfolgingstilfelle.client.id}") private val isoppfolgingstilfelleClientId: String,
+    @Value("\${ispengestopp.client.id}") private val ispengestoppClientId: String,
+    @Value("\${ispersonoppgave.client.id}") private val ispersonoppgaveClientId: String,
+    @Value("\${smregistrering.client.id}") private val smregisteringClientId: String,
+    @Value("\${syfobehandlendeenhet.client.id}") private val syfobehandlendeenhetClientId: String,
     @Value("\${syfomodiaperson.client.id}") private val syfomodiapersonClientId: String,
     @Value("\${syfooversiktsrv.client.id}") private val syfooversiktsrvClientId: String,
+    @Value("\${syfoperson.client.id}") private val syfopersonClientId: String,
     private val graphApiConsumer: GraphApiConsumer,
 ) {
     private var kode6TjenesteList = listOf(
-        smregisteringClientId,
+        fastlegerestClientId,
+        finnfastlegeClientId,
+        isdialogmeldingClientId,
         isdialogmoteClientId,
+        isdialogmotekandidatClientId,
+        isnarmestelederClientId,
+        isoppfolgingstilfelleClientId,
+        ispengestoppClientId,
+        ispersonoppgaveClientId,
+        smregisteringClientId,
+        syfobehandlendeenhetClientId,
         syfomodiapersonClientId,
         syfooversiktsrvClientId,
+        syfopersonClientId,
     )
 
     @Timed("syfotilgangskontroll_kode6TilgangSevice_harTilgang", histogram = true)
