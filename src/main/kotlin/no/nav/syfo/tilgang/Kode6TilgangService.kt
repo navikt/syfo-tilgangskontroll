1ff5a81ed355d8d9d99be66d98ea66a2270bf04f
geir-waagboe <geir.arne.waagbo@nav.no>
2022-09-13 15:14:59 +0200

    Tillate kode6-tilgang fra syfomodia

--- src/main/kotlin/no/nav/syfo/tilgang/Kode6TilgangService.kt
+++ src/main/kotlin/no/nav/syfo/tilgang/Kode6TilgangService.kt
@@ -3,31 +3,18 @@ package no.nav.syfo.tilgang
 import io.micrometer.core.annotation.Timed
 import no.nav.syfo.consumer.graphapi.GraphApiConsumer
 import no.nav.syfo.domain.AdRoller
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
 class Kode6TilgangService(
     private val adRoller: AdRoller,
-    @Value("\${smregistrering.client.id}") private val smregisteringClientId: String,
-    @Value("\${isdialogmote.client.id}") private val isdialogmoteClientId: String,
     private val graphApiConsumer: GraphApiConsumer,
 ) {
-    private var kode6TjenesteList = listOf(smregisteringClientId, isdialogmoteClientId)
 
     @Timed("syfotilgangskontroll_kode6TilgangSevice_harTilgang", histogram = true)
     fun harTilgang(
-        consumerClientId: String,
         veilederId: String
     ): Boolean {
-        return harTjenesteTilgang(consumerClientId) && harVeilederTilgang(veilederId)
-    }
-
-    private fun harTjenesteTilgang(consumerClientId: String): Boolean {
-        return kode6TjenesteList.contains(consumerClientId)
-    }
-
-    private fun harVeilederTilgang(veilederId: String): Boolean {
         return graphApiConsumer.hasAccess(veilederId, adRoller.KODE6)
     }
 }
